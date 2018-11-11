20ccc67c4d15b3995fd4b81f8c86a731941f1047
Frederik Agerkvist <s153565@student.dtu.dk>
2018-11-11 16:51:52 +0100

    Help button works - bad picture though

--- app/src/main/java/com/example/matos/trackmore3/Add_new_test2.java
+++ app/src/main/java/com/example/matos/trackmore3/Add_new_test2.java
@@ -3,6 +3,7 @@ package com.example.matos.trackmore3;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
@@ -21,35 +22,34 @@ public class Add_new_test2 extends AppCompatActivity {
         editPin = findViewById(R.id.pincode);
         editCode = findViewById(R.id.code);
 
-
-        editName.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                editName.getText().clear();
-
-            }
-        });
-
-        editPin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
-
-        editCode.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                editCode.getText().clear();
-            }
-        });
-
-
+       editName.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               editName.getText().clear();
+               setTexts();
+           }
+       });
 
     }
 
 
+    public void setTexts() {
 
 
+        editName.setOnKeyListener(new View.OnKeyListener() {
+            public boolean onKey(View view, int keyCode, KeyEvent keyevent) {
+                //If the keyevent is a key-down event on the "enter" button
+                if ( (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    editPin.getText().clear();
+                    editName.clearFocus();
+                    System.out.println("sadfghjkhgfdfghjklkjhgfdcvbnmk,lkjhgfd");
+                    editPin.requestFocus();
+                    return true;
+                }
+                return false;
+            }
+        });
+
+    }
 
 }
